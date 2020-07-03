fe4b9e50a7cf17e276aacaf45ef73ff685e4c2f3
Michael Broniola <mbroniola@gmail.com>
2020-07-03 11:52:40 -0700

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -10,15 +10,13 @@ def open_connection():
     connection = getattr(g, '_connection', None)
     if connection is None:
         connection = g._connection = sqlite3.connect(PATH)
-
     connection.row_factory = sqlite3.Row
-
     return connection
 
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
-    cursor = connection.exectue(sql, values)
+    cursor = connection.execute(sql, values)
     if commit:
         results = connection.commit()
     else:
